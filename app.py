3b9ff6d97895c3a351d961f315845e0ee6450512
KobeDR <kobe.deridder@kuleuven.be>
2026-07-29 19:50:33 +0200

    .

--- app.py
+++ app.py
@@ -61,9 +61,14 @@ if not email or not password:
     st.info("Please enter your Garmin email and password in the sidebar.")
     st.stop()
 info1 = st.info(f"Logging in {email}...")
-if "client" not in st.session_state:
+if ("client" not in st.session_state):
     client = get_client(email, password)
     st.session_state.client = client
+    st.email= email
+elif (st.email != st.email):
+    client = get_client(email, password)
+    st.session_state.client = client
+    st.email= email
 info1.empty()
 client = st.session_state.client
 if view == 'Daily':
