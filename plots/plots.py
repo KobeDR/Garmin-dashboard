5688c646d775b3d75ac249d1d8ad40535a4ae89b
KobeDR <kobe.deridder@kuleuven.be>
2026-07-31 12:57:15 +0200

    .

--- plots/plots.py
+++ plots/plots.py
@@ -117,7 +117,7 @@ def plot_year_overview2(df, year, client):
     except:
         avg_steps = "" 
     try:
-        vO2MaxValue = f"{vo2max_list[-1]}"
+        vO2MaxValue = f"{round([i for i in vo2max_list if str(i) != 'nan'][-1])}"
     except:
         vO2MaxValue = "" 
     try:
