9824870ed0d794c3f361588ef7adf794aac33993
KobeDR <kobe.deridder@kuleuven.be>
2026-07-31 09:57:38 +0200

    .

--- plots/plots.py
+++ plots/plots.py
@@ -613,7 +613,7 @@ def plot_day_overview2(df_hr, df_stress, year, month, day, client):
         for ax in (ax1, ax2, ax3, ax4):
             ax.set_xlim(td, nd)
             if ((datetime_now < nd) and (datetime_now > td)):
-                ax.axvline(x = datetime_now, color = 'black')
+                ax.axvline(x = datetime_now, color = 'black', ls = '--', lw = 0.5, label = 'now')
             
         
             
