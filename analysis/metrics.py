a5fa1e2cc38ac454ec2efeb077199b8777c18401
KobeDR <kobe.deridder@kuleuven.be>
2026-07-31 10:40:52 +0200

    .

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -4,20 +4,9 @@ METRICS = [
 
     ("restingHeartRate", "Resting HR"),
 
-    ("minAvgHeartRate", "Minimum HR"),
-
-    ("maxAvgHeartRate", "Maximum HR"),
-
     ("totalSteps", "# Steps"),
 
     ("totalDistanceMeters", "Distance (m)"),
 
     ("stressPercentage", "Stress %"),
-
-    ("averageStressLevel", "Average Stress"),
-
-    ("bodyBatteryAtWakeTime", "Body Battery morning"),
-
-    ("bodyBatteryDrainedValue", "Body Battery evening")
-
 ]
